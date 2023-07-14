edf699da7514a5bcabab7689e91fc022645f8a7c
fatih çetin <138293317+fatihcetinagu@users.noreply.github.com>
2023-07-14 18:40:21 +0300

    Add files via upload

---
+++ Matlab_main.m
@@ -0,0 +1,58 @@
+% This file is the main file of the Matlab application
+
+% test_benign
+% test_benign
+imageFolderPath = fullfile('path');
+saveFolderPath = fullfile('path');
+segmentImages(imageFolderPath, saveFolderPath);
+disp('segmentetation done!');
+
+
+imageFolderPath = fullfile('path');
+saveFolderPath = fullfile('path');
+excelFileName = 'name.xlsx';
+feature_extraction(imageFolderPath, saveFolderPath, excelFileName);
+disp('test_benign_features done!');
+
+% test_malignant
+% test_malignant
+imageFolderPath = fullfile('path');
+saveFolderPath = fullfile('path');
+segmentImages(imageFolderPath, saveFolderPath);
+disp('segmentetation done!');
+
+
+imageFolderPath = fullfile('path');
+saveFolderPath = fullfile('path');
+excelFileName = 'name.xlsx';
+feature_extraction(imageFolderPath, saveFolderPath, excelFileName);
+disp('test_malignant_features done!');
+
+% train_benign
+% train_benign
+imageFolderPath = fullfile('path');
+saveFolderPath = fullfile('path');
+segmentImages(imageFolderPath, saveFolderPath);
+disp('segmentetation done!');
+
+
+imageFolderPath = fullfile('path');
+saveFolderPath = fullfile('path');
+excelFileName = 'name.xlsx';
+feature_extraction(imageFolderPath, saveFolderPath, excelFileName);
+disp('train_benign_features done!');
+
+% train_malignant
+% train_malignant
+imageFolderPath = fullfile('path');
+saveFolderPath = fullfile('path');
+segmentImages(imageFolderPath, saveFolderPath);
+disp('segmentetation done!');
+
+
+imageFolderPath = fullfile('path');
+saveFolderPath = fullfile('path');
+excelFileName = 'name.xlsx';
+feature_extraction(imageFolderPath, saveFolderPath, excelFileName);
+disp('train_malignant_features done!');
+disp('All operations succesfuly completed');
